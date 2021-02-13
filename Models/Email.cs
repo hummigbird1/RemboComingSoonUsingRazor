9d82b3686df74212c45cd0ada0f990c6c22dc406
Mercy Tum <mercytum@gmail.com>
2021-02-14 00:54:41 +0300

    Using razor pages without MVC

---
+++ Models/Email.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemboComingSoon.Models
+{
+    public class Email
+    {
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemboComingSoon.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        [BindProperty]
+        public string EmailAddress { get; set; }
+        public void OnPost()
+        {
+            ViewData["confirmation"] = $" Thank you for signing up. Information will be sent to {EmailAddress}";
+        }
+    }
+}
