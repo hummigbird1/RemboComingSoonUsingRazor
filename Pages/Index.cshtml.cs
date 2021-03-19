1e938fd246f1f309cab6e2214f22c703047fe9ab
hummigbird1 <hummigbird1@gmail.com>
2021-03-19 21:43:06 +0100

    "Reset" the page data by redirecting to our page and basically simply "loose the previous data" This is the primarily recommended approach for your intended feature

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -45,7 +45,11 @@ namespace RemboComingSoon.Pages
                 return Page();
             }
 
-            ViewData["confirmation"] = $" Thank you for signing up. Information will be sent to {EmailAddress}";
+            // We set the confirmation in the TempData (instead of the ViewData) - TempData seems to be the more recommended way for doing this kind of thing
+            // as it appears it has autromatisms to be cleared automatically once it has been used
+            // Also it is the only way to make this work with the more commonly recommended so called "Post-Redirect-Get" (PRG) pattern
+            // Be aware though that also the HTML Part needs to use TempData now instead of the ViewData
+            TempData["confirmation"] = $" Thank you for signing up. Information will be sent to {EmailAddress}";
 
             // However since you are receiving a simple string as property now, you need to create a model for storing it in the database
             // in any way that suits your needs. 
@@ -58,7 +62,10 @@ namespace RemboComingSoon.Pages
             _emailDbContext.Email.Add(emailDbModel);
             await _emailDbContext.SaveChangesAsync();
 
-            return Page();
+            // Since we are redirecting now to our page (instead of re-rendering it) ther entire post data (bound property and modelstate) is no longer available 
+            // and therefore the page appears "reset" although in fact it is an entirely new page (as if we came to it the first time)
+            // This approach seems to be the more recommended way as it behaves properly with browser "back navigation" and reload functions
+            return RedirectToPage();
         }
     }
 }
